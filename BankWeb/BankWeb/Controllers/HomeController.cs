f7c1309dd4f51bc1d4ff005ee05a739f85070d97
Gary Murdoch <garymurdoch12@gmail.com>
2022-10-18 15:49:00 -0600

    got project ready for migration

---
+++ BankWeb/BankWeb/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using BMO_Auth.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BMO_Auth.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int table_number)
+        {
+            ViewBag.tableNumber = table_number;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
